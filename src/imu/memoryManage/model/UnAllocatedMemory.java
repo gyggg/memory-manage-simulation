5959bb485ba1dcf6375d5df0f4934c178d4a5bbf
Zyzzz <zhouyan@imudges.com>
2016-11-25 15:09:44 +0800

    已分配内存类和未分配内存类

--- src/imu/memoryManage/model/UnAllocatedMemory.java
+++ src/imu/memoryManage/model/UnAllocatedMemory.java
@@ -4,4 +4,22 @@ package imu.memoryManage.model;
  * Created by Administrator on 2016/11/24.
  */
 public class UnAllocatedMemory {
+   private int length;
+   private int startAddress;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getStartAddress() {
+        return startAddress;
+    }
+
+    public void setStartAddress(int startAddress) {
+        this.startAddress = startAddress;
+    }
 }
