6ebb16ae417795320cd7f9471261326ff7f4d890
Zyzzz <zhouyan@imudges.com>
2016-11-25 16:32:57 +0800

    内存初始化

--- src/imu/memoryManage/algorithm/MemoryManageAlogorithm.java
+++ src/imu/memoryManage/algorithm/MemoryManageAlogorithm.java
@@ -1,7 +1,62 @@
 package imu.memoryManage.algorithm;
 
+import imu.memoryManage.model.AllocatedMemory;
+import imu.memoryManage.model.UnAllocatedMemory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by guyu on 2016/11/25.
  */
 public class MemoryManageAlogorithm {
+    int [] Memory;
+    private List<UnAllocatedMemory> unAllocatedMemories = new ArrayList<>();
+    private List<AllocatedMemory> allocatedMemories = new ArrayList<>();
+
+    public int[] getMemory() {
+        return Memory;
+    }
+
+    public void setMemory(int[] memory) {
+        Memory = memory;
+    }
+
+    public List<UnAllocatedMemory> getUnAllocatedMemories() {
+        return unAllocatedMemories;
+    }
+
+    public void setUnAllocatedMemories(List<UnAllocatedMemory> unAllocatedMemories) {
+        this.unAllocatedMemories = unAllocatedMemories;
+    }
+
+    public List<AllocatedMemory> getAllocatedMemories() {
+        return allocatedMemories;
+    }
+
+    public void setAllocatedMemories(List<AllocatedMemory> allocatedMemories) {
+        this.allocatedMemories = allocatedMemories;
+    }
+
+    public void initMemory(int length){
+        Memory = new int [length];
+        UnAllocatedMemory unAllocatedMemory = new UnAllocatedMemory();
+        unAllocatedMemory.setLength(length);
+        unAllocatedMemory.setStartAddress(0);
+        unAllocatedMemories.add(unAllocatedMemory);
+    }
+
+
+    public void firstFitAlgorithm(int length){
+        for(int i = 0;i<unAllocatedMemories.size();i++){
+            if(unAllocatedMemories.get(i).getLength()>length){
+
+            }
+        }
+    }
+
+
+
+
+
 }
