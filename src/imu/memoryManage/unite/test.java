e4ed1bf9888418f287126b8b833a4a57d2d821bd
Zyzzz <zhouyan@imudges.com>
2016-11-25 15:39:09 +0800

    测试函数

--- src/imu/memoryManage/unite/test.java
+++ src/imu/memoryManage/unite/test.java
@@ -3,6 +3,7 @@ package imu.memoryManage.unite;
 import imu.memoryManage.model.UnAllocatedMemory;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,6 +12,25 @@ import java.util.List;
 public class test {
     public static void main(String[] args) {
         List<UnAllocatedMemory> unAllocatedMemories = new ArrayList<UnAllocatedMemory>();
-
+        UnAllocatedMemory allocatedMemory1 = new UnAllocatedMemory();
+        UnAllocatedMemory allocatedMemory2 = new UnAllocatedMemory();
+        UnAllocatedMemory allocatedMemory3 = new UnAllocatedMemory();
+        allocatedMemory1.setLength(5);
+        allocatedMemory2.setLength(1);
+        allocatedMemory3.setLength(7);
+        allocatedMemory1.setStartAddress(7);
+        allocatedMemory2.setStartAddress(15);
+        allocatedMemory3.setStartAddress(25);
+        unAllocatedMemories.add(allocatedMemory1);
+        unAllocatedMemories.add(allocatedMemory2);
+        unAllocatedMemories.add(allocatedMemory3);
+        Collections.sort(unAllocatedMemories);
+        for(int i=0;i<3;i++){
+            System.out.print(unAllocatedMemories.get(i).getLength());
+        }
+        Collections.reverse(unAllocatedMemories);
+        for(int i=0;i<3;i++){
+            System.out.print(unAllocatedMemories.get(i).getLength());
+        }
     }
 }
